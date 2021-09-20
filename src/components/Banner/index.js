babd8fbf5ebd99972403e33d41435a01d1f85791
Deepak <deepak42844@gmail.com>
2021-09-20 14:19:51 +0530

    Banner component added , font-aswsome is added

---
+++ src/components/Banner/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Banner = () => {
+    return (
+        <>
+        <div className="d-sm-flex justify-content-sm-end bg-dark text-white p-2">
+
+            <div className="">
+                <i className="fa fa-envelope-o" aria-hidden="true"></i>
+                <span>adv.dks@gmail.com</span>
+            </div>
+            <div className="">
+                <i className="fa fa-phone-square" aria-hidden="true"></i>
+                <span>1800-526-562</span>
+            </div>
+        
+            
+        </div>
+        </>
+    )
+}
