f8b206d301b46508f3957b9fa4f452e111d65d75
Deepak <deepak42844@gmail.com>
2021-09-20 22:34:30 +0530

    image updates, banner modefied

--- src/components/Navigationbar/index.js
+++ src/components/Navigationbar/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 const NavigationBar = () => {
   return (
@@ -21,55 +21,6 @@ const NavigationBar = () => {
       </Container>
     </Navbar>
   );
-
-  // return (
-  //   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  //     <div className="container">
-  //       <a className="navbar-brand" href="#home">
-  //         Icon & Title
-  //       </a>
-  //       <button
-  //         className="navbar-toggler"
-  //         type="button"
-  //         data-bs-toggle="collapse"
-  //         data-bs-target="#navbarSupportedContent"
-  //         aria-controls="navbarSupportedContent"
-  //         aria-expanded="false"
-  //         aria-label="Toggle navigation"
-  //         onClick={() => setShow(!show)}
-  //       >
-  //         <span className="navbar-toggler-icon"></span>
-  //       </button>
-  //       <div
-  //         className={`collapse navbar-collapse ${show ? 'show' : ''}`}
-  //         id="navbarSupportedContent"
-  //       >
-  //         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-  //           <li className="nav-item">
-  //             <a className="nav-link active" aria-current="page" href="#home">
-  //               Home
-  //             </a>
-  //           </li>
-  //           <li className="nav-item">
-  //             <a className="nav-link" href="#associates">
-  //               Associates
-  //             </a>
-  //           </li>
-  //           <li className="nav-item">
-  //             <a className="nav-link" href="#cases">
-  //               About
-  //             </a>
-  //           </li>
-  //           <li className="nav-item">
-  //             <a className="nav-link" href="#cases">
-  //               Login
-  //             </a>
-  //           </li>
-  //         </ul>
-  //       </div>
-  //     </div>
-  //   </nav>
-  // );
 };
 
 export default NavigationBar;
