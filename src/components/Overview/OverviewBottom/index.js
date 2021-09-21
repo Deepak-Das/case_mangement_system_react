363943dd8a243e6589261833f56299fa05eadd28
Deepak <deepak42844@gmail.com>
2021-09-21 21:49:00 +0530

    added overviewBottom

---
+++ src/components/Overview/OverviewBottom/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function OverviewBottom() {
+  return (
+    <>
+      <div className="container bg-info text-center text-uppercase py-5">
+        <h3 className="fs-1 f-">experence associates</h3>
+      </div>
+    </>
+  );
+}
+
+export default OverviewBottom;
--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -1,13 +1,15 @@
 import React from "react";
+import OverviewBottom from "./OverviewBottom";
 import OverviewMid from "./OverviewMid";
 import OverviewUpper from "./OverviewUpper";
 
 export const Overview = () => {
   return (
     <>
-      <section className="container-fluid border-bottom border-1 border-dark">
+      <section className="container-fluid border-bottom border-1 border-dark mb-5">
         <OverviewUpper />
         <OverviewMid />
+        <OverviewBottom />
       </section>
     </>
   );
